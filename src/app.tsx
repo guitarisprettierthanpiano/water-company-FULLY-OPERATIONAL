b11fd627f91aa30c0f2021b817e7322b8725daa3
guitarisprettierthanpiano <shfabrizio@gmail.com>
2021-04-11 03:50:53 -0400

    setting up the app file

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
+import { StripeProvider, Elements} from 'react-stripe-elements';
+import Form from './form'
 
 function App(){
     return (
         <>
-            <h1>HI</h1>
+            <h1>HI2</h1>
+            <StripeProvider apiKey='pk_test_51IexqXEi1Zp5FPtuuyn808cPjyqahIaTxoidZLaIKkTDyagZKdhRQq0WYJkPUUzfibiLsVNwnqcGpOzo4IcLb74u0062FlK49x'>
+                <Elements>
+                    <Form />
+                </Elements>
+            </StripeProvider>
         </>
     )
 }
---
+++ src/form.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { CardElement, injectStripe, ReactStripeElements } from 'react-stripe-elements';
+
+function Form() {
+
+    return(
+        <h1>Form</h1>
+    )
+}
+
+export default Form;
